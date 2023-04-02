59c27e19cc5b0b16693582ae8174629bd671433f
ti1ek <tilek.koszhanov@mail.ru>
2023-04-02 16:24:39 +0500

    Project Setup

---
+++ colors-list-MVVM/Sources/App/SceneDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  SceneDelegate.swift
+//  colors-list-MVVM
+//
+//  Created by Tilek Koszhanov on 02.04.2023.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let sceneWindow = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: sceneWindow)
+        let colorsListViewController = ColorsListViewController()
+        window?.rootViewController = colorsListViewController
+        window?.makeKeyAndVisible()
+    }
+}
---
+++ colors-list-MVVM/Sources/View/ColorsListViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ColorsListViewController.swift
+//  colors-list-MVVM
+//
+//  Created by Tilek Koszhanov on 02.04.2023.
+//
+
+import UIKit
+
+class ColorsListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
